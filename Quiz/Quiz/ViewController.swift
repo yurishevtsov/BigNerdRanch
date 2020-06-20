9f2206e851c58851f6816e49418273c6da9c184d
Yuri Shevtsov <yuri.shevtsov@mobiledefense.com>
2020-06-20 13:21:43 -0400

    First chapter

---
+++ Quiz/Quiz/ViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ViewController.swift
+//  Quiz
+//
+//  Created by Yuri Shevtsov on 6/13/20.
+//  Copyright © 2020 Yuri Shevtsov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet var questionLabel: UILabel!
+    @IBOutlet var answerLabel: UILabel!
+    
+    let questions: [String] = [
+        "What is 7+7?",
+        "What is the capital of Vermont?",
+        "What is cognac made from?"
+    ]
+    let answers: [String] = [
+    "`what the fuck`",
+    "Montpelier",
+    "Grapes"
+    ]
+    var currentQuestionIndex: Int = 0
+    
+    @IBAction func showNextQuestion(_ sender: UIButton) {
+        currentQuestionIndex += 1
+        if currentQuestionIndex == questions.count {
+            currentQuestionIndex = 0
+        }
+        
+        let question: String = questions[currentQuestionIndex]
+        questionLabel.text = question
+        answerLabel.text = "???"
+    }
+    
+    @IBAction func showAnswer(_ sender: UIButton) {
+        let answer: String = answers[currentQuestionIndex]
+        answerLabel.text = answer
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        questionLabel.text = questions[currentQuestionIndex]
+    }
+}

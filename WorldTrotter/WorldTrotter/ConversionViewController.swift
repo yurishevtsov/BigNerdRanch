300a47a0727f8285529d0557a705967d4a58f579
Yuri Shevtsov <yuri.shevtsov@mobiledefense.com>
2020-06-28 17:35:17 -0400

    Silver challenge - cleaned up

--- WorldTrotter/WorldTrotter/ConversionViewController.swift
+++ WorldTrotter/WorldTrotter/ConversionViewController.swift
@@ -9,37 +9,13 @@
 import UIKit
 
 class ConversionViewController: UIViewController {
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//
-//        let firstFrame = CGRect(x: 160, y: 240, width: 100, height: 150)
-//        let firstView = UIView(frame: firstFrame)
-//        firstView.backgroundColor = UIColor.blue
-//        view.addSubview(firstView)
-//
-//        let secondFrame = CGRect(x: 20, y: 30, width: 50, height: 50)
-//        let secondView = UIView(frame: secondFrame)
-//        secondView.backgroundColor = UIColor.green
-//        firstView.addSubview(secondView)
-//    }
-
     
-    
-        override func viewDidLoad() {
-            super.viewDidLoad()
+    override func viewDidLoad() {
+        super.viewDidLoad()
 
-//            view.backgroundColor = UIColor(red: .random(in: 0...1), green: .random(in: 0...1), blue: .random(in: 0...1), alpha: 1.0)
-            print("ConversionViewController loaded its view.")
+        print("ConversionViewController loaded its view.")
     }
-    
-//    override func viewWillAppear(_ animated: Bool) {
-//        super.viewWillAppear(animated)
-//        print("viewWillAppear(_:) called")
-//        
-//        view.backgroundColor = UIColor(red: .random(in: 0...1), green: .random(in: 0...1), blue: .random(in: 0...1), alpha: 1.0)
-//    }
-    
-    
+     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
 

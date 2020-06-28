c504cdcb1793b40634be853403365dabcf32278f
Yuri Shevtsov <yuri.shevtsov@mobiledefense.com>
2020-06-28 18:00:00 -0400

    Silver challenge - added functions

--- WorldTrotter/WorldTrotter/ConversionViewController.swift
+++ WorldTrotter/WorldTrotter/ConversionViewController.swift
@@ -13,56 +13,42 @@ class ConversionViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        func createLabelWithProps(labelText: String,
+                                  rgbColor: UInt = 0x000000,
+                                  fontSize: CGFloat = 36) -> UILabel {
+            let label = UILabel()
+            label.text = labelText
+            label.textAlignment = .center
+            label.textColor = UIColor(rgb: rgbColor, alphaVal: 1.0)
+            label.font = .systemFont(ofSize: fontSize)
+            label.translatesAutoresizingMaskIntoConstraints = false
+            view.addSubview(label)
+            return label
+        }
+        
+        func centerLabelInView(label: UILabel) {
+            label.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        }
+        
         // Create all labels and define their properties
-        let fValueLabel = UILabel()
-        fValueLabel.text = "202"
-        fValueLabel.textAlignment = .center
-        fValueLabel.textColor = UIColor(rgb: 0xE15829, alphaVal: 1.0)
-        fValueLabel.font = .systemFont(ofSize: 70)
-        fValueLabel.translatesAutoresizingMaskIntoConstraints = false
-
-        let fDescriptionLabel = UILabel()
-        fDescriptionLabel.text = "degrees Fahrenheit"
-        fDescriptionLabel.textColor = UIColor(rgb: 0xE15829, alphaVal: 1.0)
-        fDescriptionLabel.font = .systemFont(ofSize: 36)
-        fDescriptionLabel.translatesAutoresizingMaskIntoConstraints = false
-        
-        let isReallyLabel = UILabel()
-        isReallyLabel.text = "is really"
-        isReallyLabel.font = .systemFont(ofSize: 36)
-        isReallyLabel.translatesAutoresizingMaskIntoConstraints = false
-        
-        let cValueLabel = UILabel()
-        cValueLabel.text = "100"
-        cValueLabel.textColor = UIColor(rgb: 0xE15829, alphaVal: 1.0)
-        cValueLabel.font = .systemFont(ofSize: 70)
-        cValueLabel.translatesAutoresizingMaskIntoConstraints = false
-        
-        let cDescriptionLabel = UILabel()
-        cDescriptionLabel.text = "degrees Celcius"
-        cDescriptionLabel.textColor = UIColor(rgb: 0xE15829, alphaVal: 1.0)
-        cDescriptionLabel.font = .systemFont(ofSize: 36)
-        cDescriptionLabel.translatesAutoresizingMaskIntoConstraints = false
-        
-        // Add labels to the view
-        view.addSubview(fValueLabel)
-        view.addSubview(cValueLabel)
-        view.addSubview(fDescriptionLabel)
-        view.addSubview(cDescriptionLabel)
-        view.addSubview(isReallyLabel)
+        let fValueLabel = createLabelWithProps(labelText: "202", rgbColor: 0xE15829, fontSize: 70)
+        let fDescriptionLabel = createLabelWithProps(labelText: "degrees Fahrengeit", rgbColor: 0xE15829)
+        let isReallyLabel = createLabelWithProps(labelText: "is really")
+        let cValueLabel = createLabelWithProps(labelText: "100", rgbColor: 0xE15829, fontSize: 70)
+        let cDescriptionLabel = createLabelWithProps(labelText: "degrees Celcius", rgbColor: 0xE15829)
         
         // Define all the necessary contraints
         fValueLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 12).isActive = true
         fDescriptionLabel.topAnchor.constraint(equalTo: fValueLabel.bottomAnchor, constant: 12).isActive = true
         isReallyLabel.topAnchor.constraint(equalTo: fDescriptionLabel.bottomAnchor, constant: 12).isActive = true
-        cValueLabel.topAnchor.constraint(equalTo: isReallyLabel.bottomAnchor, constant: 8).isActive = true
+        cValueLabel.topAnchor.constraint(equalTo: isReallyLabel.bottomAnchor, constant: 12).isActive = true
         cDescriptionLabel.topAnchor.constraint(equalTo: cValueLabel.bottomAnchor, constant: 12).isActive = true
         
-        fValueLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        fDescriptionLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        isReallyLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        cValueLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        cDescriptionLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        centerLabelInView(label: fValueLabel)
+        centerLabelInView(label: fDescriptionLabel)
+        centerLabelInView(label: isReallyLabel)
+        centerLabelInView(label: cValueLabel)
+        centerLabelInView(label: cDescriptionLabel)
     }
 }
 

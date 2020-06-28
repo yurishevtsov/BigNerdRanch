d1b9f2fb5b3d0f6b5f443725b4997a6fd5ce15b5
Yuri Shevtsov <yuri.shevtsov@mobiledefense.com>
2020-06-28 11:08:55 -0400

    Chapter 5

--- WorldTrotter/WorldTrotter/MapViewController.swift
+++ WorldTrotter/WorldTrotter/MapViewController.swift
@@ -7,16 +7,61 @@
 //
 
 import UIKit
+import MapKit
 
 class MapViewController: UIViewController {
     
-        override func viewDidLoad() {
-            super.viewDidLoad()
-            
-            print("MapViewController loaded its view.")
+    var mapView: MKMapView!
+    
+    override func loadView() {
+        // create a map view
+        mapView = MKMapView()
+        
+        // set it as THE view of this view controller
+        view = mapView
+        
+        let segmentedControl = UISegmentedControl(items: ["Standard", "Hybrid", "Satellite"])
+        segmentedControl.backgroundColor = UIColor.systemBackground
+        segmentedControl.selectedSegmentIndex = 0
+        
+        segmentedControl.addTarget(self, action: #selector(mapTypeChanged(_:)), for: .valueChanged)
+        
+        segmentedControl.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(segmentedControl)
+        
+        let topConstrain = segmentedControl.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 8)
+        
+        let margins = view.layoutMarginsGuide
+        let leadingConstraint = segmentedControl.leadingAnchor.constraint(equalTo: margins.leadingAnchor)
+        let trailingConstrain = segmentedControl.trailingAnchor.constraint(equalTo: margins.trailingAnchor)
+        
+        topConstrain.isActive = true
+        leadingConstraint.isActive = true
+        trailingConstrain.isActive = true
+        
+
     }
     
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+            
+        print("MapViewController loaded its view.")
+    }
+    
+    @objc func mapTypeChanged(_ segControl: UISegmentedControl) {
+        switch segControl.selectedSegmentIndex {
+        case 0:
+            mapView.mapType = .standard
+        case 1:
+            mapView.mapType = .hybrid
+        case 2:
+            mapView.mapType = .satellite
+        default:
+            break
+        }
+    }
 
     
 }

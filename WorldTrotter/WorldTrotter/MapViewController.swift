eb0f5f0e240177e731c10cdbdc8b7debf2404dd3
Yuri Shevtsov <yuri.shevtsov@mobiledefense.com>
2020-06-28 12:16:08 -0400

    Bronze challenge

--- WorldTrotter/WorldTrotter/MapViewController.swift
+++ WorldTrotter/WorldTrotter/MapViewController.swift
@@ -40,13 +40,40 @@ class MapViewController: UIViewController {
         leadingConstraint.isActive = true
         trailingConstrain.isActive = true
         
-
+        let poiLabel = UILabel()
+        poiLabel.text = "Points of Interest"
+        
+        poiLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(poiLabel)
+        
+        let labelTopConstrain = poiLabel.topAnchor.constraint(equalTo: segmentedControl.bottomAnchor, constant: 25)
+        let labelLeadingConstrain = poiLabel.leadingAnchor.constraint(equalTo: margins.leadingAnchor)
+        
+        labelTopConstrain.isActive = true
+        labelLeadingConstrain.isActive = true
+        
+        
+        let poiSwitch = UISwitch()
+        
+        poiSwitch.addTarget(self, action: #selector(pointsOfInterestFilter(_:)), for: .valueChanged)
+        
+        poiSwitch.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(poiSwitch)
+        
+        let switchTopConstrain = poiSwitch.topAnchor.constraint(equalTo: segmentedControl.bottomAnchor, constant: 25)
+        let switchLeadingConstrain = poiSwitch.leadingAnchor.constraint(equalTo: poiLabel.trailingAnchor, constant: 25)
+        
+        switchTopConstrain.isActive = true
+        switchLeadingConstrain.isActive = true
     }
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
             
+        mapView.pointOfInterestFilter = .excludingAll
         print("MapViewController loaded its view.")
     }
     
@@ -60,6 +87,20 @@ class MapViewController: UIViewController {
             mapView.mapType = .satellite
         default:
             break
+        }
+    }
+    
+    @objc func pointsOfInterestFilter(_ switchControl: UISwitch) {
+        if (switchControl.isOn) {
+            print("PoI enabled")
+//            mapView.showsPointsOfInterest = true
+            mapView.pointOfInterestFilter = .includingAll
+//            mapView.pointOfInterestFilter?.includes(.airport)
+            
+        } else {
+            print("PoI disabled")
+//            mapView.showsPointsOfInterest = false
+            mapView.pointOfInterestFilter = .excludingAll
         }
     }
 

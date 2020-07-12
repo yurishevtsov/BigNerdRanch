d0543cdd69faf00e49e68e2d6ef4d398f9a99536
Yuri Shevtsov <yuri.shevtsov@mobiledefense.com>
2020-07-11 23:07:45 -0400

    Chapter 9

---
+++ LootLogger/LootLogger/ItemStore.swift
@@ -0,0 +1,47 @@
+//
+//  ItemStore.swift
+//  LootLogger
+//
+//  Created by Yuri Shevtsov on 7/11/20.
+//  Copyright © 2020 Yuri Shevtsov. All rights reserved.
+//
+
+import UIKit
+
+class ItemStore {
+    var allItems = [Item]()
+    
+    @discardableResult func createItem() -> Item {
+        let newItem = Item(random: true)
+        
+        allItems.append(newItem)
+        
+        return newItem
+    }
+    
+    func removeItem(_ item: Item) {
+        if let index = allItems.firstIndex(of: item) {
+            allItems.remove(at: index)
+        }
+    }
+    
+    func moveItem(from fromIndex: Int, to toIndex: Int) {
+        if fromIndex == toIndex {
+            return
+        }
+        
+        // Get reference to object being moved so you can revert it
+        let movedItem = allItems[fromIndex]
+        
+        // Remove item from array
+        allItems.remove(at: fromIndex)
+        
+        // Insert item in array at new location
+        allItems.insert(movedItem, at: toIndex)
+    }
+//    init() {
+//        for _ in 0..<5 {
+//            createItem()
+//        }
+//    }
+}
---
+++ LootLogger/LootLogger/ItemsViewController.swift
@@ -0,0 +1,95 @@
+//
+//  ItemsViewController.swift
+//  LootLogger
+//
+//  Created by Yuri Shevtsov on 7/11/20.
+//  Copyright © 2020 Yuri Shevtsov. All rights reserved.
+//
+
+import UIKit
+
+class ItemsViewController: UITableViewController {
+    // Add a property for an ItemStore
+    var itemStore: ItemStore!
+    
+    @IBAction func addNewItem(_ sender: UIButton) {
+//        // Make a new index path for the 0th section, last row
+//        let lastRow = tableView.numberOfRows(inSection: 0)
+//        let indexPath = IndexPath(row: lastRow, section: 0)
+//
+//        // Insert this new row into the table
+//        tableView.insertRows(at: [indexPath], with: .automatic)
+        if isEditing {
+            return
+        }
+        
+        // Create a new item and add it to the store
+        let newItem = itemStore.createItem()
+        
+        // Figure out where that item is in the array
+        if let index = itemStore.allItems.firstIndex(of: newItem) {
+            let indexPath = IndexPath(row: index, section: 0)
+            
+            // Insert this row into the table
+            tableView.insertRows(at: [indexPath], with: .automatic)
+        }
+    }
+    
+    @IBAction func toggleEditingMode(_ sender: UIButton) {
+        // If you're currently in editing mode...
+        if isEditing {
+            // Change text of button to inform user of state
+            sender.setTitle("Edit", for: .normal)
+            
+            // Turn off editing mode
+            setEditing(false, animated: true)
+        } else {
+            // Change text of button to inform user of state
+            sender.setTitle("Done", for: .normal)
+            
+            setEditing(true, animated: true)
+            // Enter editing mode
+        }
+    }
+    
+    // Implement required data source methods of UITableViewDataSource protocol
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemStore.allItems.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // Create instance of UITableViewCell with default appearance (w/o reusing UITableViewCells)
+        //let cell = UITableViewCell(style: .value1, reuseIdentifier: "UITableViewCell")
+        
+        // Get a new or recycled cell
+        let cell = tableView.dequeueReusableCell(withIdentifier: "UITableViewCell", for: indexPath)
+        
+        // Set the text on the cell with the description of the item
+        // that is at the nth index of items, where n = row this cell
+        // will appear in on the table view
+        let item = itemStore.allItems[indexPath.row]
+        
+        cell.textLabel?.text = item.name
+        cell.detailTextLabel?.text = "\(item.valueInDollars)"
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        // If the table view is asking to commit a delete command...
+        if editingStyle == .delete {
+            let item = itemStore.allItems[indexPath.row]
+            
+            //Remove the item from the store
+            itemStore.removeItem(item)
+            
+            //Asle remove that row from the table  view with animation
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        // Update the model
+        itemStore.moveItem(from: sourceIndexPath.row, to: destinationIndexPath.row)
+    }
+}

00344d2def89e5820a2c9dabb14153e1823a60bd
Yuri Shevtsov <yuri.shevtsov@mobiledefense.com>
2020-07-19 15:58:27 -0400

    Chapter 13

--- LootLogger/LootLogger/ItemStore.swift
+++ LootLogger/LootLogger/ItemStore.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ItemStore {
     var allItems = [Item]()
+    let itemArchiveURL: URL = {
+        let documentsDirectories = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        let documentDirectory = documentsDirectories.first!
+
+        return documentDirectory.appendingPathComponent("items.plist")
+    }()
     
     @discardableResult func createItem() -> Item {
         let newItem = Item(random: true)
@@ -44,4 +50,33 @@ class ItemStore {
 //            createItem()
 //        }
 //    }
+    
+    @objc func saveChanges() -> Bool {
+        print("Saving items to: \(itemArchiveURL)")
+        
+        do {
+            let encoder = PropertyListEncoder()
+            let data = try encoder.encode(allItems)
+            try data.write(to: itemArchiveURL, options: [.atomic])
+            print("Saved all of the items")
+            return true
+        } catch let encodingError {
+            print("Error encoding allItems: \(encodingError)")
+            return false
+        }
+    }
+    
+    init() {
+        do {
+            let data = try Data(contentsOf: itemArchiveURL)
+            let unarchiver = PropertyListDecoder()
+            let items = try unarchiver.decode([Item].self, from: data)
+            allItems = items
+        } catch {
+            print("Error reading in saved items: \(error)")
+        }
+        
+        let notificationCenter = NotificationCenter.default
+        notificationCenter.addObserver(self, selector: #selector(saveChanges), name: UIScene.didEnterBackgroundNotification, object: nil)
+    }
 }
